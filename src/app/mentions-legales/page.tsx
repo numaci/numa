2471f373436f61da2b4ecbb9f5cbab55eba92296
numaci <numa4476@gmail.com>
2025-12-17 00:51:31 +0000

    Update legal mentions with new owner details

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -4,11 +4,11 @@ export default function MentionsLegales() {
   return (
     <div className="max-w-3xl mx-auto py-12 px-4">
       <h1 className="text-3xl font-bold mb-6">Mentions légales</h1>
-      <p><b>Propriétaire du site :</b> Bour sine fall</p>
-      <p><b>Adresse :</b> Sikasso</p>
-      <p><b>Email :</b>sinebour63@gmail.com</p>
-      <p><b>Téléphone :</b> +223 84054004</p>
-      <p><b>SIRET :</b> 84054000</p>
+      <p><b>Propriétaire du site :</b> Dosso Souleymane</p>
+      <p><b>Adresse :</b> Cote D'Ivoire</p>
+      <p><b>Email :</b>Dosso070000@gmail.com</p>
+      <p><b>Téléphone :</b> +225 0712150062</p>
+      <p><b>SIRET :</b> 0584220157</p>
       <p><b>Hébergeur :</b> OVH</p>
       <h2 className="text-xl font-semibold mt-8 mb-2">Propriété intellectuelle</h2>
       <p>Le contenu du site (textes, images, logo, etc.) est protégé par le droit d’auteur. Toute reproduction est interdite sans autorisation.</p>
